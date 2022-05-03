eb86d4a250df03b8e11124b6d2eb24f176bef018
Beata Tokarska <tokarskabb@gmail.com>
2022-05-03 12:33:16 +0200

    functions added in JS

--- js/script.js
+++ js/script.js
@@ -1,39 +1,58 @@
-console.log("Siema");
+{
+    const welcome = () => {
+        console.log("Siema, jest to mój pierwszy projekt i zupełne początki z JS :)")
 
-let photoButton = document.querySelector(".photo__button");
+    }
+
+    const onChangeBackgroundClick = () => {
+        const body = document.querySelector(".body");
+        const themeNameDark = document.querySelector(".themeNameDark");
+
+        body.classList.toggle("dark");
+        themeNameDark.innerText = body.classList.contains("dark") ? "jasny" : "ciemny"
+
+    };
+
+    const onChangeSize = () => {
+        const mainText__aboutMe = document.querySelector(".mainText__aboutMe");
+        const themeNameBiggerLetters = document.querySelector(".themeNameBiggerLetters");
+
+        mainText__aboutMe.classList.toggle("biggerLetters");
+        themeNameBiggerLetters.innerText = body.classList.contains("biggerLetters") ? "zmniejsz" : "powieksz"
+
+    };
+
+    const init = () => {
+        const buttonDark = document.querySelector(".mainText__buttonDark");
+        buttonDark.addEventListener("click", onChangeBackgroundClick);
+
+        const buttonBiggerLetters = document.querySelector(".mainText__buttonBiggerLetters");
+
+        buttonBiggerLetters.addEventListener("click", onChangeSize);
+
+        welcome();
+        onRemovePhoto();
+    };
+
+    const onRemovePhoto = () => {
+        const photoButton = document.querySelector(".photo__button");
+
+        const photo = document.querySelector(".photo");
+        photoButton.addEventListener("click", () => {
+            photo.remove();
+        });
+
+
+    }
+
+
+    init();
+}
+
+
 let header = document.querySelector(".header");
-let photo = document.querySelector(".photo");
-photoButton.addEventListener("click", () => {
-    photo.remove();
-});
 
 
-
-let buttonBiggerLetters = document.querySelector(".mainText__buttonBiggerLetters");
-let mainText__aboutMe = document.querySelector(".mainText__aboutMe");
-let themeNameBiggerLetters = document.querySelector(".themeNameBiggerLetters");
-
-buttonBiggerLetters.addEventListener("click", () => {
-    mainText__aboutMe.classList.toggle("biggerLetters");
-    if (mainText__aboutMe.classList.contains("biggerLetters")) {
-        themeNameBiggerLetters.innerText = "zmniejsz";
-    } else {
-        themeNameBiggerLetters.innerText = "powiększ";
-    }
-});
-
-let buttonDark = document.querySelector(".mainText__buttonDark");
-let body = document.querySelector(".body");
-let themeNameDark = document.querySelector(".themeNameDark");
-
-buttonDark.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    if (body.classList.contains("dark")) {
-        themeNameDark.innerText = "jasny";
-    } else {
-        themeNameDark.innerText = "ciemny";
-    }
-});
 
 
 
